4d5db624a6215dc6af39dd7a0666d6abf2279106
kef <kefeer@brokestream.com>
2009-12-29 17:21:55 +0500

    move udp out of main

--- ll.c
+++ ll.c
@@ -201,11 +201,53 @@ uint64_t kick(uint64_t f) {
 	return 0;
 }
 
-int main(int argc,char *argv[]) {
-	int s=socket(AF_INET, SOCK_DGRAM, 0);
+int s=-1;
+
+void udp() {
+	unsigned char b[1024];
+	if(recv(s,b,1024,0)>0) {
+		uint64_t sn=*(uint64_t*)(b+3);
+		int n=find(sn);
+		int l=*(uint16_t*)(b+1);
+
+		switch(b[0]){
+		case 'A':
+		case 'D':
+			types[n]=b[0];
+			addrs[n]=realloc(addrs[n],lens[n]=l);
+			memcpy(addrs[n],b+11,l);
+			break;
+		case 'F':
+			types[n]=b[0];
+			addrs[n]=realloc(addrs[n],lens[n]=l*8+5);
+			{
+				uint64_t *p=(uint64_t*)(b+11);
+				uint64_t *d=(uint64_t*)(addrs[n]+5);
+				makeitforth(addrs[n]);
+				int i; for(i=0;i<l;i++) {
+					if((char)*p) { *d=*p; } else { *d=find(*p)<<8; }
+					d++; p++;
+				}
+			}
+			break;
+		case 'E':
+			llcall(addrs[n]);
+			break;
+		default:;
+		}
+	}
+}
+
+void udpinit() {
+	s=socket(AF_INET, SOCK_DGRAM, 0);
 	struct sockaddr_in a={.sin_family=AF_INET,.sin_port=htons(1233),.sin_addr={htonl(0x7f000001)}};
 	bind(s,(struct sockaddr*)&a,sizeof(a));
 	fcntl(s, F_SETFL, O_NONBLOCK);
+}
+
+int main(int argc,char *argv[]) {
+	udpinit();
+
 	memset(names,0,sizeof(names));
 	memset(addrs,0,sizeof(addrs));
 	memset(lens,0,sizeof(lens));
@@ -215,42 +257,10 @@ int main(int argc,char *argv[]) {
 	if(argc>1) { savename=argv[1]; }
 	load(); dump();
 
-
 	llsp--;
 
 	for(;;) {
-		unsigned char b[1024];
-		if(recv(s,b,1024,0)>0) {
-			uint64_t sn=*(uint64_t*)(b+3);
-			int n=find(sn);
-			int l=*(uint16_t*)(b+1);
-
-			switch(b[0]){
-			case 'A':
-			case 'D':
-				types[n]=b[0];
-				addrs[n]=realloc(addrs[n],lens[n]=l);
-				memcpy(addrs[n],b+11,l);
-				break;
-			case 'F':
-				types[n]=b[0];
-				addrs[n]=realloc(addrs[n],lens[n]=l*8+5);
-				{
-					uint64_t *p=(uint64_t*)(b+11);
-					uint64_t *d=(uint64_t*)(addrs[n]+5);
-					makeitforth(addrs[n]);
-					int i; for(i=0;i<l;i++) {
-						if((char)*p) { *d=*p; } else { *d=find(*p)<<8; }
-						d++; p++;
-					}
-				}
-				break;
-			case 'E':
-				llcall(addrs[n]);
-				break;
-			default:;
-			}
-		}
+		udp();
 		llcall(addrs[0]);
 	}
 }
